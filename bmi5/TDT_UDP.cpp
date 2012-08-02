e84b7ec5f0a8c456bbabc99b6c4ed1cd6004df8f
Timothy Hanson <sideskate@gmail.com>
2012-08-01 20:07:44 -0700

    starting with communication with the the RZ2 via UDP, based on Flips work with the VizServer.

---
+++ bmi5/TDT_UDP.cpp
@@ -0,0 +1,156 @@
+#include "TDT_UDP.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h> // for inet_addr
+#include <netdb.h> 
+#include <fcntl.h> 
+
+#define CP_printf printf
+#define INVALID_SOCKET 0
+
+
+
+int openSocket(char *strIPAddr){
+	sockaddr_in sin;
+	int sock= INVALID_SOCKET;
+
+	// prepare the SIN to send to
+	memset(&sin, 0, sizeof(sockaddr_in));
+
+	sin.sin_family = AF_INET;
+	// sin.sin_addr.S_un.S_addr = ipAddr; //inet_addr("tdt_udp_0000000");
+	sin.sin_addr.s_addr = inet_addr(strIPAddr);
+
+	sin.sin_port = htons(LISTEN_PORT);
+
+	CP_printf("UDP: Attempting to open UDP %s\n", strIPAddr);
+
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+	CP_printf("... socket created\n");
+	if (sock == INVALID_SOCKET){
+		fprintf(stderr, "Failed to create the UDP socket: \n"); 
+	perror(":");
+	}
+	int optval = 1; // turn on address reuse. 
+setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)); 
+
+	// Flip uses connect rather than bind here -- so outgoing packets have a default endpoint.
+	if (connect(sock, (struct sockaddr*)&sin, sizeof(sockaddr_in)) != 0)
+	{
+		close(sock);
+		sock = INVALID_SOCKET;
+		fprintf(stderr,"Failed to connect the UDP socket:\n"); 
+		perror(":");
+	}
+	CP_printf("... done\n");
+
+	return sock;
+}
+
+//sets up a UDP/TCP server socket. 
+int setup_socket(int portno){
+	int sock;
+	struct sockaddr_in serv_addr;
+
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sock < 0) {
+		fprintf(stderr, "ERROR opening socket\n");
+		return 0; 
+	}
+	fcntl(sock, F_SETFL, O_NONBLOCK); //set the socket to non-blocking. 
+	int optval = 1; // turn on address reuse. 
+	setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)); 
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY ; 
+	serv_addr.sin_port = htons(portno);
+	if(bind(sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0){
+		fprintf(stderr,"error binding socket \n");
+		perror(":");
+	}
+	listen(sock, 5); 
+	return sock;
+}
+
+bool checkRZ(int sock, char *strIPAddr)
+{
+	sockaddr_in from;
+	from.sin_family = AF_INET;
+	from.sin_addr.s_addr = inet_addr(strIPAddr);
+	from.sin_port = htons(LISTEN_PORT);
+	char req[HEADER_BYTES] = COMMAND_HEADER(GET_VERSION), resp[1024];
+	//if( sendto(sock, req, HEADER_BYTES, 0, (sockaddr*)&from, sizeof(from))){
+	if (send(sock, req, HEADER_BYTES, 0) != HEADER_BYTES){
+		fprintf(stderr, "Failed to send GET_VERSION packet:\n"); 
+		perror(":");
+	}
+
+	if (recv(sock, resp, 1024, 0) != 4 ||
+		memcmp(req, resp, 3) != 0 ||
+		resp[3] != 1)
+		return false;
+
+	char packet[HEADER_BYTES] = COMMAND_HEADER(SET_REMOTE_IP);
+	if (send(sock, packet, HEADER_BYTES, 0) != HEADER_BYTES){
+		fprintf(stderr, "Failed to send \"start sending\" packet:\n"); 
+		perror(":");
+	}
+	//TODO: does it reply? discard packet if it does.
+
+	return true;
+}
+
+
+
+bool sendData(int sock, double *data, int count)
+{
+	char *packet;
+    bool retval = false;
+	int i;
+
+	// Allocate packet
+	packet = new char[4+4*count];
+	
+	// Get header and load into packet
+	char header[4] = DATA_HEADER((unsigned char)count);
+	for(i=0; i<4; i++) packet[i]=header[i];
+
+	// Load data (as floats)
+    char offset = 0;
+    for(i=0; i<count; i++)
+		((float*)(packet + HEADER_BYTES))[i] = (float)(data[i]);
+
+    // match the byte ordering of the RZ communication
+    uint32_t *udata = (uint32_t*)(packet + HEADER_BYTES);
+    for(i=0; i<count; i++)
+        udata[i] = htonl(udata[i]);
+
+    // send the data over the UDP connection
+    if (send(sock, packet, 4 + count * 4, 0) == 4 + count * 4)
+        retval = true;
+
+	// Close
+		  free(packet); 
+    return retval;
+}
+
+void disconnectRZ(int sock)
+{
+    if (sock != INVALID_SOCKET)
+    {
+        char packet[HEADER_BYTES] = COMMAND_HEADER(FORGET_REMOTE_IP);
+        if (send(sock, packet, HEADER_BYTES, 0) != HEADER_BYTES){
+            fprintf(stderr, "Failed to send \"stop sending\" packet: %d\n");
+		  		perror(":");
+	 		}
+
+        close(sock);
+        sock = INVALID_SOCKET;
+    }
+}
---
+++ bmi5/main.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "TDT_UDP.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <math.h>
+#include <time.h>
+#include <arpa/inet.h>
+
+double g_startTime = 0.0;
+
+double gettime(){ //in seconds!
+	timespec pt ;
+	clock_gettime(CLOCK_MONOTONIC, &pt);
+	double ret = (double)(pt.tv_sec) ;
+	ret += (double)(pt.tv_nsec) / 1e9 ;
+	return ret - g_startTime;
+	//printf( "present time: %d s %d ns \n", pt.tv_sec, pt.tv_nsec ) ;
+}
+
+int main(int argn, char** argc){
+	int sock; 
+ 	sock = openSocket("169.230.191.195"); 
+	//sock = setup_socket(LISTEN_PORT); // joey's RZ2. 
+	checkRZ(sock, "169.230.191.195"); 
+	unsigned char buf[1024]; 
+	double mean = 0; 
+	double var = 0; 
+	int np = 1; 
+	g_startTime = gettime(); 
+	double last = 0; 
+	unsigned int oldpak = 0; 
+	unsigned int dropped = 0; 
+	while(1){
+		//currently the socket is blocking -- will wait for data.
+		unsigned int n = recv(sock, buf, 1024, 0);
+		if(n > 0 && n < 1024){
+			double now = gettime(); 
+ 			np++; 
+			double d = now - last; 
+			mean += d; 
+			double m = (mean / (double)np); 
+			var += (d - m)*(d - m);
+			//look for lost packets. 
+			unsigned int paknum = htonl(*(unsigned int*)(&(buf[4])));  
+			if(paknum - oldpak > 1 && paknum - oldpak < 20) 
+				dropped += paknum - oldpak; 
+			oldpak = paknum;
+			printf("%x bytes: %d", n); 
+			for(int i=0; i<n; i++){
+				printf("%02x ", buf[i]); 
+			}
+			printf("\n"); 
+			printf("mean interpacket time %f std %f dropped %d\n", 
+					 m, sqrt(var / (double)np), dropped); 
+			last = now; 
+		}
+	}
+	return 0; 
+}
