bcff0c1475ea9a9b20b72ef98c249bd549ad8a2f
Joseph O'Doherty <joseph.odoherty@gmail.com>
2013-02-13 19:33:36 -0800

    bmi5: tim refactored bmi5_mmap to fix a bug in which stale data was being presented as if it were fresh

--- bmi5/bmi5_mmap.cpp
+++ bmi5/bmi5_mmap.cpp
@@ -6,63 +6,93 @@
 #define BMI5_IN_FIFO	"/tmp/bmi5_in.fifo"
 #define BMI5_OUT_FIFO	"/tmp/bmi5_out.fifo"
 
+#define B5_IN 1
+#define B5_OUT 2
+#define B5_IO (B5_IN | B5_OUT)
+
+
+void doMmap(const mxArray *prhs[], double *b, bool read, int nfields, char* out){
+	for(int f=0; f<nfields; f++){
+		mxArray* field = mxGetFieldByNumber(prhs[0], 0, f); 
+		if(field){
+			int m = mxGetM(field); 
+			int n = mxGetN(field);
+			if(0){
+				const char* fieldname = mxGetFieldNameByNumber(prhs[0], f); 
+				mexPrintf("%s is %d by %d ", fieldname, m, n); 
+				if(out[f]) mexPrintf("out "); 
+				else mexPrintf("in "); 
+			}
+			double* data = (double*)mxGetData(field); 
+			if(data){
+				for(int j=0; j<m*n; j++){
+					if(read){ //matlab -> bmi5
+						if(out[f] & B5_IN)
+							*b++ = data[j]; 
+						else *b++; //keep pointer in sync.
+					} else { //bmi5 -> matlab
+						if(out[f] & B5_OUT)
+							data[j] = *b++; 
+						else *b++; 
+					}
+				}
+			}
+		}
+	}
+}
+
+char* getInOut(const mxArray *prhs[ ], int nfields){
+	char *out = (char*)malloc(nfields * sizeof(bool)); 
+	for(int f=0; f<nfields; f++){
+		const char* fieldname = mxGetFieldNameByNumber(prhs[0], f); 
+		out[f] = B5_IN; 
+		for(int j=0; j<256 && fieldname[j]; j++){
+			if(fieldname[j] == '_' && fieldname[j+1] == 'o')
+				out[f] = B5_OUT; 
+			if(fieldname[j] == '_' && fieldname[j+1] == 'i' && fieldname[j+2] == 'o')
+				out[f] = B5_IO; 
+		}
+	}
+	return out; 
+}
+
 void mexFunction( int nlhs, mxArray *plhs[ ], int nrhs, const mxArray *prhs[ ] ) {
 	//matlab built-in mmap funtionality too slow (scales with # of variables). 
 	//we can do better.
 	//call this mex function to pass commands; returns new values; 
 	//does not do synchronization (at present). 
 	size_t length = 256*8;
-	mmapHelp mmh(length, BMI5_CTRL_MMAP, false); // probably too much time here..
-	double* b = (double*)mmh.m_addr; // but matlab does not easily support persistent data structures. 
-	
+	int ret = 0; 
 	if(nrhs == 1){
+		mmapHelp mmh(length, BMI5_CTRL_MMAP, false); // probably too much time here..
+		double* b = (double*)mmh.m_addr; // but matlab does not easily support persistent data structures. 
 		int nfields = mxGetNumberOfFields(prhs[0]); 
-		for(int f=0; f<nfields; f++){
-			const char* fieldname = mxGetFieldNameByNumber(prhs[0], f); 
-			bool out = false; 
-			for(int j=0; j<256 && fieldname[j]; j++){
-				if(fieldname[j] == '_' && fieldname[j+1] == 'o')
-					out = true; 
-			}
-			mxArray* field = mxGetFieldByNumber(prhs[0], 0, f); 
-			if(field){
-				int m = mxGetM(field); 
-				int n = mxGetN(field);
-				//mexPrintf("%s is %d by %d\n", fieldname, m, n); 
-				double* data = (double*)mxGetData(field); 
-				if(data){
-					if(!out){
-						for(int j=0; j<m*n; j++){
-							*b++ = data[j]; 
-						}
-					}else{
-						for(int j=0; j<m*n; j++)
-							data[j] = *b++;
-					}
-				}
-			}
+		char* out = getInOut(prhs, nfields); 
+	// INCOMING DATA!
+		doMmap(prhs, b, true, nfields, out); 
+	//sync here, to save time.
+		int pipe_in = open(BMI5_IN_FIFO, O_RDWR); 
+		if(pipe_in <= 0){
+			char buf[256];
+			getcwd(buf, 256); 
+			buf[255] = 0; 
+			mexPrintf("bmi5_mmap: could not open fifo: %s; %s\n", BMI5_IN_FIFO, buf); 
+		}else{
+			write(pipe_in, (void*)"go.", 3); 
+			close(pipe_in); 
 		}
+		int pipe_out = open(BMI5_OUT_FIFO, O_RDWR); 
+		if(pipe_out <= 0){
+			mexPrintf("bmi5_mmap: could not open fifo: %s\n",BMI5_OUT_FIFO); 
+		}else{
+			read(pipe_out, (void*)&ret, sizeof(int)); 
+			close(pipe_out); 
+		}
+	// OUTGOING DATA!
+		doMmap(prhs, b, false, nfields, out); 
+		free(out); 
 	}else{
 		mexPrintf("bmi5_mmap: please supply the b5 structure as an argument.\n"); 
-	}
-	//sync here, to save time.
-	int pipe_in = open(BMI5_IN_FIFO, O_RDWR); 
-	if(pipe_in <= 0){
-		char buf[256];
-		getcwd(buf, 256); 
-		buf[255] = 0; 
-		mexPrintf("bmi5_mmap: could not open fifo: %s; %s\n", BMI5_IN_FIFO, buf); 
-	}else{
-		write(pipe_in, (void*)"go.", 3); 
-		close(pipe_in); 
-	}
-	int pipe_out = open(BMI5_OUT_FIFO, O_RDWR); 
-	int ret = 0; 
-	if(pipe_out <= 0){
-		mexPrintf("bmi5_mmap: could not open fifo: %s\n",BMI5_OUT_FIFO); 
-	}else{
-		read(pipe_out, (void*)&ret, sizeof(int)); 
-		close(pipe_out); 
 	}
 	if(nlhs == 1){
 		plhs[0] = mxCreateDoubleMatrix(1,1,mxREAL);
