7c43ce756d2f1cf95172ec02bced8dc7959af77c
Joseph O'Doherty <joseph.odoherty@gmail.com>
2013-02-19 10:51:09 -0800

    fixed some bad-merge garbage

--- bmi5/matlab/bmi5_control_test.m
+++ bmi5/matlab/bmi5_control_test.m
@@ -11,11 +11,7 @@ bmi5_in  = fopen('/tmp/bmi5_in.fifo',  'w');
 % initial settings -- setup a cursor and a starfield.
 bmi5_cmd('store float 3 trial'); 
 bmi5_cmd('make circle cursor'); 
-<<<<<<< HEAD:bmi5/matlab/bmi5_control_test.m
 bmi5_cmd('make stars stars'); 
-=======
-bmi5_cmd('make dots stars'); 
->>>>>>> 4f73b166fb8437baea8101f86f86baf8f8de10f8:bmi5/bmi5_control_test.m
 bmi5_cmd('make circle target'); 
 bmi5_cmd('make tone tone');  % tone object, not actual tone.
 eval(bmi5_cmd('mmap structure')); 
