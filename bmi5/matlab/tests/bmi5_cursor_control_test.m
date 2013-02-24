477af7861b6a58afb4446162af3be80d58173817
Joseph O'Doherty <joseph.odoherty@gmail.com>
2013-02-24 00:02:13 -0800

    fleshing out cursor control test

--- bmi5/matlab/tests/bmi5_cursor_control_test.m
+++ bmi5/matlab/tests/bmi5_cursor_control_test.m
@@ -1,17 +1,29 @@
-function [] = bmi5_cursor_control_test()
-global b5; 
-load('calibration.mat'); % load pm & q. 
-affine = eye(4); 
-affine(1:2, 1:2) = q(1:2, 1:2)'; 
-affine(1:2, 4) = q(3, 1:2)'; 
-b5.affine_m44 = affine; % so everything is saved in world coords.
-b5.stars_scale = [10; 10]; % units: cm!
-b5.cursor_scale = [1; 1]; 
-b5.target_scale = [1; 1]; 
+global bmi5_out bmi5_in b5
+
+bmi5_out = fopen('/tmp/bmi5_out.fifo', 'r');
+bmi5_in  = fopen('/tmp/bmi5_in.fifo',  'w');
+
+bmi5_cmd('make circle cursor');
+bmi5_cmd('polhemus finger');
+eval(bmi5_cmd('mmap structure'));
+
+b5.cursor_scale = [5; 5];       % in mm
+b5.cursor_color = [1; 1; 1; 1]; % white
+b5.cursor_pos   = [0; 0];
+b5.cursor_draw  = 1;
+
+load('../calibration.mat'); % load pm & q. 
+
+qp = q'; 
+q2 = eye(4); 
+q2(1:2, 1:2) = qp(1:2, 1:2); 
+q2(1:2, 4) = qp(1:2, 4); 
+b5.affine_m44 = q2; 
+
 bmi5_mmap(b5); 
 
 while(1)
-    world = pm * b5.polhemus_sensors_o; 
-    b5.cursor_pos = world(1:2); 
+    p = pm * b5.finger_sensors_o; 
+    b5.cursor_pos = p(1:2); 
     bmi5_mmap(b5);
 end
