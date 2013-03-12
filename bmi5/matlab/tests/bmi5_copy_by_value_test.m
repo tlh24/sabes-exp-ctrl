eb1b2a71fd35cb1682627507db6b7204b3831833
Joseph O'Doherty <joseph.odoherty@gmail.com>
2013-03-12 00:52:05 -0700

    copy by value test

---
+++ bmi5/matlab/tests/bmi5_copy_by_value_test.m
@@ -0,0 +1,26 @@
+clear;
+global bmi5_out bmi5_in
+
+bmi5_out = fopen('/tmp/bmi5_out.fifo', 'r');
+bmi5_in  = fopen('/tmp/bmi5_in.fifo',  'w');
+eval(bmi5_cmd('mmap'));
+
+foo(1).time = 0;
+
+fprintf('*** fill structure ***\n');
+for i=1:5
+
+    bmi5_mmap(b5);
+    foo(i).time = b5.time_o;  % get the time
+
+    fprintf('foo(%d).time: %0.2f\n',i,foo(i).time);
+
+    pause(1);
+end
+
+fprintf('\n\n');
+
+fprintf('*** print structure ***\n');
+for i=1:5
+    fprintf('foo(%d).time: %0.2f\n',i,foo(i).time);
+end
